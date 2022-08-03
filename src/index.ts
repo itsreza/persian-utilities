744a12defc8bef44cf61bc3edd658eeb8174e1a3
its.reza <itsrezababakhani@gmail.com>
2022-08-03 18:48:44 +0430

    Add WordifyNumbers Method Without CleanUp

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import persianCardValidation from './modules/creditCardValidation';
 import { convertDigitsFaToEn, convertDigitsEnToFa } from './shared/utils/digitConverter/index';
 import { timeAgo } from './modules/timeAgo';
 import { default as isNationalID } from './modules/nationalIDValidation';
-// import {default as numberToWord } from "./modules/numericalToWords"
+import {default as wordifyNumbers } from "./modules/numericalToWords"
 import { numericalSeparator , numericalWithoutSeparator} from "./shared/utils/numerical"
 
 
-console.log(timeAgo("۱۴۰۱/۰۵/۰۹ ۱۰:۰۰" , false))
 
-export { persianCardValidation, convertDigitsFaToEn, convertDigitsEnToFa, timeAgo, isNationalID  , numericalSeparator , numericalWithoutSeparator };
+
+export { persianCardValidation, convertDigitsFaToEn, convertDigitsEnToFa, timeAgo, isNationalID  , numericalSeparator , numericalWithoutSeparator , wordifyNumbers };
--- src/modules/numericalToWords/index.ts
+++ src/modules/numericalToWords/index.ts
@@ -1,17 +1,133 @@
-const numericalToWords = (number: number) => {
+import { UNITS_WORDS_NUMERICAL , HUNDRED_WORDS_NUMERICAL  } from "../../shared/constants/wordsAndNumerical/index.constants"
+
+const convertUnitNumbers = (pureNumber)=> {
+    const result: Array<string> = [];
+    if(!pureNumber){
+        return ""
+    }
+    if(UNITS_WORDS_NUMERICAL?.[pureNumber]){
+        result.push(UNITS_WORDS_NUMERICAL?.[pureNumber])
+   } else {
+       const splittedNumber = String(pureNumber).split("")
+       result.push(UNITS_WORDS_NUMERICAL?.[Number(splittedNumber[0] + 0)])
+       result.push(UNITS_WORDS_NUMERICAL?.[splittedNumber[1]])
+   }
+   return result.join(" و ")
+}
+
+const convertHundredNumbers = (pureNumber)=> {
+    const unitNumber = pureNumber % 100;
+    const hundredNumber = HUNDRED_WORDS_NUMERICAL?.[Math.floor(pureNumber - unitNumber)]
+    // console.log(pureNumber, unitNumber , pureNumber - unitNumber)
+    //     const hundredNumber = HUNDRED_WORDS_NUMERICAL?.[Math.floor(pureNumber - unitNumber)]
+        return unitNumber ? `${hundredNumber} و ${convertUnitNumbers(unitNumber)}` : hundredNumber;
+}
+
+const convertThousandNumbers = (pureNumber)=> {
+    // const unitNumber = pureNumber % 1000;
+    // const thousandNumber = unitNumber / 1000
+    // return !unitNumber ? `${convertHundredNumbers(thousandNumber)} هزار` : "a"
+    // const hundredNumber = HUNDRED_WORDS_NUMERICAL?.[Math.floor(pureNumber - unitNumber)]
+    // return unitNumber ? `${convertHundredNumbers} و ${convertUnitNumbers(unitNumber)}` : hundredNumber;
+    // const hundredNumber = pureNumber % 1000;
+    // return `${convertHundredNumbers(Math.floor(pureNumber / 1000))} هزار و ${convertHundredNumbers(hundredNumber)}`
+}
+
+const getFixedAndRemainingNumber = (number: number , separate): {remainingNumber: number ; fixedNumber: number}=> {
+    const remainingNumber = number % separate;
+    const fixedNumber = Math.floor(number / separate);
+    return {remainingNumber , fixedNumber}
+    // console.log(remainingNumber)
+    // return numericalToWords(remainingNumber)
+}
+
+const result: Array<string> = [];
+function numericalToWords(number: number) {
 if(!number){
 throw new Error("Persian-Utilities Error : Number Should be Pass to numericalToWord Method.");
 }
 // Number Converted
 let pureNumber = number;
-const result: Array<string> = [];
+
+
+
+if(pureNumber <= 99){
+    // Convert 2 Digits Numbers (UNITS)
+    const convertedUnitNumberToPersianWord = convertUnitNumbers(pureNumber)
+    result.push(convertedUnitNumberToPersianWord)
+} else if (pureNumber <= 999){
+    // Convert 3 Digits Numbers (HUNDREDS)
+    const convertedHundredNumberToPersianWord = convertHundredNumbers(pureNumber)
+    result.push(convertedHundredNumberToPersianWord)
+} else if (pureNumber <= 999999){
+    // Convert 6 Digits Numbers (THOUSANDS)
+    // wordifyBigNumbers(pureNumber , "هزار")
+    const {remainingNumber , fixedNumber } = getFixedAndRemainingNumber(pureNumber, 1000)
+    numericalToWords(fixedNumber)
+    result.push("هزار و")
+    if(remainingNumber){
+        numericalToWords(remainingNumber)
+    }
+} else if (pureNumber <= 999999999){
+    // 1000000
+    // Convert 9 Digits Numbers (MILLIONS)
+    const {remainingNumber , fixedNumber } = getFixedAndRemainingNumber(pureNumber, 1000000)
+    numericalToWords(fixedNumber)
+    result.push("میلیون و")
+    if(remainingNumber){
+        numericalToWords(remainingNumber)
+    }
+} else if (pureNumber <= 999999999999){
+    // Convert 12 Digits Numbers (Billion)
+    const {remainingNumber , fixedNumber} = getFixedAndRemainingNumber(pureNumber , 1000000000)
+    numericalToWords(fixedNumber)
+    result.push("میلیارد و")
+    if(remainingNumber){
+        numericalToWords(remainingNumber)
+    }
+} else {
+     throw new Error("Persian-Utilities : Your Number Has been overflowed...")
+}
+
+// if(pureNumber < 999999){
+//     const unitNumber = pureNumber % 1000;
+//     const hundredNumber = Math.floor((pureNumber - unitNumber) / 1000);
+//     // const hundredNumber = HUNDRED_WORDS_NUMERICAL?.[]
+//     // console.log({hundredNumber})
+//     // console.log({unitNumber , a :  , hundredNumber})
+//     console.log(convertHundredNumbers(unitNumber))
+//     return convertHundredNumbers(hundredNumber)
+// }
+
+// if(pureNumber <= 999999999){
+//     const unitNumber = pureNumber % 1000;
+//     const a = pureNumber % 1000000;
+//     const hundredNumber = Math.floor((pureNumber - unitNumber) / 1000000);
+//     console.log(convertHundredNumbers(unitNumber))
+//     console.log(convertHundredNumbers(a))
+//     return convertHundredNumbers(hundredNumber)
+// }
+
+// console.log({result})
+// function swap(json){
+//     var ret = {};
+//     for(var key in json){
+//       ret[json[key]] = key;
+//     }
+//     return ret;
+//   }
+//  console.log( swap(UNITS_WORDS_NUMERICAL))
+// if(pureNumber < 10){
+//     result.push()
+// }
+
 // if(pureNumber < 10){
 //     pureNumber = number - 1;
 //     result.push("yek")
 // }
 
-
-return pureNumber % 10
+// console.log({result})
+return result.join("")
 }
 
 export default numericalToWords;
@@ -35,3 +151,63 @@ export default numericalToWords;
 // } else if (num < 1000000000000000) {
 //     result += wordifyfa(Math.floor(num / 1000000000000), level) + " تریلیارد" + wordifyfa(num % 1000000000000, level + 1);
 // }
+
+
+
+// actual  conversion code starts here
+
+// var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+// var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+// var teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+// function convert_millions(num) {
+//   if (num >= 1000000) {
+//     return convert_millions(Math.floor(num / 1000000)) + " million " + convert_thousands(num % 1000000);
+//   } else {
+//     return convert_thousands(num);
+//   }
+// }
+
+// function convert_thousands(num) {
+//   if (num >= 1000) {
+//     return convert_hundreds(Math.floor(num / 1000)) + " thousand " + convert_hundreds(num % 1000);
+//   } else {
+//     return convert_hundreds(num);
+//   }
+// }
+
+// function convert_hundreds(num) {
+//   if (num > 99) {
+//     return ones[Math.floor(num / 100)] + " hundred " + convert_tens(num % 100);
+//   } else {
+//     return convert_tens(num);
+//   }
+// }
+
+// function convert_tens(num) {
+//   if (num < 10) return ones[num];
+//   else if (num >= 10 && num < 20) return teens[num - 10];
+//   else {
+//     return tens[Math.floor(num / 10)] + " " + ones[num % 10];
+//   }
+// }
+
+// function convert(num) {
+//   if (num == 0) return "zero";
+//   else return convert_millions(num);
+// }
+
+// //end of conversion code
+
+// //testing code begins here
+
+// function main() {
+//   var cases = [0, 1, 2, 7, 10, 11, 12, 13, 15, 19, 20, 21, 25, 29, 30, 35, 50, 55, 69, 70, 99, 100, 101, 119, 510, 900, 1000, 5001, 5019, 5555, 10000, 11000, 100000, 199001, 1000000, 1111111, 190000009];
+//   for (var i = 0; i < cases.length; i++) {
+//     console.log(cases[i] + ": " + convert(cases[i]));
+//   }
+// }
+
+// main();
+
+
